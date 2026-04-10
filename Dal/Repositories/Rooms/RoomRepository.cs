8acc89d3b3d922345a8c3a20fb8919b513024ad6
Vearsmon <vearsmon285537@gmail.com>
2026-04-10 11:33:05 +0500

    fix

--- Dal/Repositories/Rooms/RoomRepository.cs
+++ Dal/Repositories/Rooms/RoomRepository.cs
@@ -2,6 +2,7 @@
 using Dal.Transactions;
 using Domain.Models;
 using Domain.Models.SearchModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dal.Repositories.Rooms;
 
@@ -20,5 +21,11 @@ public class RoomRepository(
     public async Task<bool> ExistsAsync(Guid id)
     {
         return (await base.SelectAsync([id])).Length == 1;
+    }
+
+    protected override IQueryable<DbRoom> Query()
+    {
+        return Context.Set<DbRoom>()
+            .Include(x => x.Campus);
     }
 }
